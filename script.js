ac20cec0cd91b3fecc09c18637b354431ec87d90
RufatIsgander <rufet.isr12345@gmail.com>
2022-11-17 12:18:41 -0800

    add, remove

---
+++ script.js
@@ -0,0 +1,55 @@
+// let form = document.querySelector(".form");
+// let btn = document.querySelector("button");
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     let tr = document.createElement("tr");
+//     let td = document.createElement("td");
+//     let deleteBtn = document.createElement("button");
+
+//     deleteBtn.innerText = "X"
+//     deleteBtn.classList.add("delete");
+//     let tbody = document.querySelector("tbody");
+//     tbody.append(tr);
+//     tr.append(td, deleteBtn);
+//     td.innerText = event.target.username.value;
+
+// })
+// let delBtn = document.querySelectorAll(".delete");
+
+
+// delBtn.addEventListener("click", (event) => {
+//     let tbody = document.querySelector("tbody");
+//     let tr = document.createElement("tr");
+//     tbody.removeChild(event.target.tr);
+// })
+
+let form = document.querySelector(".form");
+let btn = document.querySelector(".submit-btn");
+
+btn.addEventListener("click", () => {
+    if (form.input.value.length == 0) {
+        alert("Enter plan");
+    }
+    else {
+        document.querySelector(".tasks").innerHTML +=
+            ` <div class="task">
+        <span id="taskname">
+        ${document.querySelector('form input').value}
+        </span>
+        <button class="delete">
+        X
+        </button>
+        </div>`;
+    }
+    // document.querySelector(".tasks").classList.add("tasks");
+    form.input.value = "";
+
+    let tasks = document.querySelectorAll(".delete");
+    tasks.forEach(element => {
+        element.addEventListener("click", (event) => {
+            event.target.parentNode.remove();
+        })
+    });
+
+})
+
